1930959782f9acb408954bd48d61ee7b1a311f31
Sadi <gazisagor00000@gmail.com>
2021-07-24 03:03:51 +0600

    addes spinner on home page

--- zest-shopping-client/src/pages/Home.js
+++ zest-shopping-client/src/pages/Home.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Container, Row, Col, InputGroup, FormControl, Button } from 'react-bootstrap';
 import Product from '../components/Product/Product';
+import loader from '../Images/loader.gif'
 
 const Home = () => {
     const [products, setProducts] = useState([])
@@ -22,6 +23,11 @@ const Home = () => {
                 </Col>
                <Row>
                     {
+                        products.length === 0 ?
+                        <div className="text-center mt-3">
+                            <img src={`${loader}`} alt="" style={{height: '300px'}}/>
+                        </div>
+                        :
                         products.map(data => <Product data={data} key={data.id}/>)
                     }
                </Row>
