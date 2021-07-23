ae384feb2b08ce04368b1fe06cab1172401acdf9
Sadi <gazisagor00000@gmail.com>
2021-07-23 16:55:22 +0600

    initial commit

---
+++ zest-shopping-client/src/App.js
@@ -0,0 +1,12 @@
+import { Button } from 'react-bootstrap';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Button>Hello</Button>
+    </>
+  );
+}
+
+export default App;
---
+++ zest-shopping-client/src/config/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBCgcTxwnsVA1DSJqIm-z4sl7pB-JUgX1o",
+    authDomain: "zest-shopping.firebaseapp.com",
+    projectId: "zest-shopping",
+    storageBucket: "zest-shopping.appspot.com",
+    messagingSenderId: "301055383890",
+    appId: "1:301055383890:web:61e27d3451f351f834028b"
+  };
